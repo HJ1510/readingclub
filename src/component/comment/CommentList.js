dc055425a2bb53bfaacd58a77523297d06d0555f
HJ1510 <yhj221004@gmail.com>
2023-02-14 16:36:41 +0900

    branch create

--- src/component/comment/CommentList.js
+++ src/component/comment/CommentList.js
@@ -1,5 +1,39 @@
 import "../../assets/css/component/comment/Comment.css";
 
+const styles = {
+  wrapper: {
+    margin: 8,
+    padding: 8,
+    display: "flex",
+    flexDirection: "row",
+    border: "1px solid grey",
+    borderRadius: 16,
+  },
+  imageContainer: {},
+  image: {
+    width: 130,
+    borderRadius: 25,
+  },
+  contextContainer: {
+    marginLeft: 8,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  nameText: {
+    color: "black",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  commentTest: {
+    color: "black",
+    fontSize: 16,
+  },
+  button: {
+    float: "left",
+  },
+};
+
 function formatDate(value) {
   const date = new Date(value);
   return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
@@ -9,15 +43,23 @@ function CommentListItem({ item, onDelete }) {
   const deleteCommentClick = () => onDelete(item.id);
 
   return (
-    <div>
-      {/* <img src={item.imgUrl} alt={item.title} /> */}
-      <div>
-        <span>
+    <div style={styles.wrapper}>
+      <div style={styles.imageContainer}>
+        <img
+          src={item.imgUrl}
+          style={styles.image}
+          alt={item.title}
+        />
+      </div>
+      <div style={styles.contextContainer}>
+        <div>
           <h4>{item.title}</h4>
-        </span>
-        <span>{item.content}</span>
-        <span>{item.rating}</span>
-        <span>{formatDate(item.createdAt)}</span>
+        </div>
+        <div>{item.content}</div>
+        <div>{item.rating}</div>
+        <div>{formatDate(item.createdAt)}</div>
+      </div>
+      <div>
         <button onClick={deleteCommentClick}>삭제</button>
       </div>
     </div>
@@ -30,9 +72,9 @@ function CommentList({ items, onDelete }) {
       <ul>
         {items.map((item) => {
           return (
-            <li key={item.id}>
+            <div key={item.id}>
               <CommentListItem item={item} onDelete={onDelete} />
-            </li>
+            </div>
           );
         })}
       </ul>
