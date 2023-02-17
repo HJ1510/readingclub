a56632874cbafd45ec968e71c268e31ecd04ef37
HJ1510 <yhj221004@gmail.com>
2023-02-17 15:28:37 +0900

    back-link

--- src/component/meeting/MeetingCreate.js
+++ src/component/meeting/MeetingCreate.js
@@ -4,38 +4,13 @@ import Col from "react-bootstrap/Col";
 import "../../assets/css/component/meeting/Meeting.css";
 import { useCallback, useState } from "react";
 
-const checkboxList = ["글쓰기", "토론"];
+// const checkboxList = ["글쓰기", "토론"];
+const meetingTypeList = ["글쓰기", "토론"];
+const meetingCategoryList = ["소설", "시", "인문"];
 
 function MeetingCreate() {
-  const [checkedList, setCheckedList] = useState([]);
-  const [isChecked, setIsChecked] = useState(false);
-
-  const checkedItemHandler = (value, isChecked) => {
-    if (isChecked) {
-      setCheckedList((prev) => [...prev, value]);
-      return;
-    }
-    if (!isChecked && checkedList.includes(value)) {
-      setCheckedList(checkedList.filter((item) => item !== value));
-      return;
-    }
-
-    return;
-  };
-
-  const checkedHandler = (e, value) => {
-    setIsChecked(!isChecked);
-    checkedItemHandler(value, e.target.checked);
-    console.log(`checkedHandler+${(value, e.target.checked)}`);
-  };
-
-  const onSubmit = useCallback(
-    (e) => {
-      e.preventDefault();
-      console.log("checkedList:", checkboxList);
-    },
-    [checkboxList]
-  );
+  const [isCheckedType, setIsCheckedType] = useState("false");
+  const [isCheckedCategory, setIsCheckedCategory] = useState("false");
 
   return (
     <Layout>
@@ -44,44 +19,82 @@ function MeetingCreate() {
           <input type="text" placeholder="모임명"></input>
         </Col>
       </Row>
-      <form onSubmit={onSubmit}>
+      <form /*onSubmit=""*/>
         <Row>
-          {checkboxList.map((item, idx) => (
+          <Col>
+            모임방식
+            {meetingTypeList.map((item, idx) => (
+              <div key={idx}>
+                <input
+                  type="checkbox"
+                  id={item}
+                  checked={true}
+                  // checked="" onChange=""
+                />
+                <label htmlFor={item}>{item}</label>
+              </div>
+            ))}
+          </Col>
+        </Row>
+      </form>
+      <Row>
+        <Col>
+          카테고리
+          {meetingCategoryList.map((item, idx) => (
             <div key={idx}>
               <input
                 type="checkbox"
                 id={item}
-                checked={checkboxList.includes(item)} // 여기부터 봐야할것
-                onChange={(e) => checkedHandler(e, item)}
+                // checked="" onChange=""
               />
               <label htmlFor={item}>{item}</label>
             </div>
           ))}
-        </Row>
-      </form>
-      <Row>
-        <Col>카테고리</Col>
+        </Col>
       </Row>
       <Row>
-        <Col>해시태크</Col>
+        <Col>
+          <input type="text" placeholder="해시태그" />
+        </Col>
       </Row>
       <Row>
-        <Col>장소</Col>
+        <Col>
+          <input type="text" placeholder="장소" />
+        </Col>
       </Row>
       <Row>
-        <Col>인원</Col>
+        <Col>
+          <input type="text" placeholder="인원" />
+        </Col>
       </Row>
       <Row>
-        <Col>소개</Col>
+        <Col>
+          <textarea name="content" placeholder="내용을 입력하세요" />
+          <button>파일업로드</button>
+        </Col>
       </Row>
       <Row>
-        <Col>일정</Col>
+        <Col>
+          < />
+        </Col>
       </Row>
       <Row>
-        <Col>성별 공개 여부</Col>
+        <Col>
+          성별
+          <input type="checkbox"></input>
+          <label>공개</label>
+          <input type="checkbox"></input>
+          <label>비공개</label>
+        </Col>
       </Row>
       <Row>
-        <Col>나이 공개 여부</Col>
+        <Col>
+          나이
+          <input type="checkbox"></input>
+          <label>공개</label>
+          <input type="checkbox"></input>
+          <label>비공개</label>
+        </Col>
       </Row>
 
       <Row>
