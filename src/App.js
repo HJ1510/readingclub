a86e7ef4990ef39ebd8779e77519e23f7cdf0489
HJ1510 <yhj221004@gmail.com>
2023-02-21 19:13:22 +0900

    fix: scroll

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./assets/css/App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Main from "./component/main/index";
 import Community from "./component/community";
 import Meeting from "./component/meeting";
@@ -19,28 +19,26 @@ import MeetingCreate2 from "component/meeting/MeetingCreate2";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/join" element={<Join />} />
-        <Route path="meeting">
-          <Route index element={<Meeting />} />
-          <Route path=":no" element={<MeetingInfo />} />
-          <Route path=":no/:id" element={<ArticleView />} />
-          <Route path="write" element={<ArticleWrite />} />
-          <Route path=":no/:id/modi" element={<ArticleModiForm />} />
-          <Route path="createmeeting" element={<MeetingCreate2 />} />
-        </Route>
-        <Route path="community" element={<Community />} />
-        <Route path="booknote">
-          <Route index element={<Booknote />} />
-          <Route path="mynote" element={<Note />} />
-          <Route path="writebook" element={<Writebook />} />
-          <Route path=":no" element={<Booknoteno></Booknoteno>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/join" element={<Join />} />
+      <Route path="meeting">
+        <Route index element={<Meeting />} />
+        <Route path=":no" element={<MeetingInfo />} />
+        <Route path=":no/:id" element={<ArticleView />} />
+        <Route path="write" element={<ArticleWrite />} />
+        <Route path=":no/:id/modi" element={<ArticleModiForm />} />
+        <Route path="createmeeting" element={<MeetingCreate2 />} />
+      </Route>
+      <Route path="community" element={<Community />} />
+      <Route path="booknote">
+        <Route index element={<Booknote />} />
+        <Route path="mynote" element={<Note />} />
+        <Route path="writebook" element={<Writebook />} />
+        <Route path=":no" element={<Booknoteno></Booknoteno>} />
+      </Route>
+    </Routes>
   );
 };
 
