d50128c9fa02c4eb7483c44f53059ec9a03702c9
HJ1510 <yhj221004@gmail.com>
2023-02-23 10:35:34 +0900

    feat: member profile

--- src/component/meeting/MeetingGroup.js
+++ src/component/meeting/MeetingGroup.js
@@ -2,11 +2,13 @@ import Board from "./board";
 import Layout from "layout/Layout";
 import { meetingList } from "MeetigData";
 import MeetingCalender from "./MeetingCalender";
-import { Col, Container, Row, Card } from "react-bootstrap";
+import { Col, Container, Row, Card, ProgressBar } from "react-bootstrap";
+import profile from "assets/images/profile.png";
 
 function MeetingGroup() {
+  const now = 60;
   return (
-    <Layout>
+    <Layout className="meeting">
       <Container>
         <h5>모임원들이 보는 모임페이지</h5>
         <h2>모임명</h2>
@@ -30,9 +32,54 @@ function MeetingGroup() {
         </Row>
         <Row>
           <Col>
-            <p>참여자</p>
+            <div className="member">
+              <img src={profile} />
+              참여자1
+              <ProgressBar
+                className="attendanceBar"
+                now={now}
+                label={`${now}%`}
+              />
+            </div>
+            <div className="member">
+              <img src={profile} />
+              참여자2
+              <ProgressBar
+                className="attendanceBar"
+                now={now}
+                label={`${now}%`}
+              />
+            </div>
+            <div className="member">
+              <img src={profile} />
+              참여자3
+              <ProgressBar
+                className="attendanceBar"
+                now={now}
+                label={`${now}%`}
+              />
+            </div>
+            <div className="member">
+              <img src={profile} />
+              참여자4
+              <ProgressBar
+                className="attendanceBar"
+                now={now}
+                label={`${now}%`}
+              />
+            </div>
+            <div className="member">
+              <img src={profile} />
+              참여자5
+              <ProgressBar
+                className="attendanceBar"
+                now={now}
+                label={`${now}%`}
+              />
+            </div>
           </Col>
         </Row>
+        <Row></Row>
         <Row>
           <Board title="모임원 게시판" />
         </Row>
