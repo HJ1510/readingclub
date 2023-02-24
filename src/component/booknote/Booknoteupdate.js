09154a41f40b2c209bef3b01c1090e8f158489af
zzzmm21 <ghdfhr4353@gmail.com>
2023-02-24 13:56:39 +0900

    독서노트 만드는중

---
+++ src/component/booknote/Booknoteupdate.js
@@ -0,0 +1,124 @@
+import React, { useState, useEffect } from "react";
+import { noteList } from "../../actions/borad_action";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import "../../assets/css/component/note/WirteEdbook.css";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import axios from "axios";
+import { Button, Form } from "react-bootstrap";
+import Libray from "./searchbook";
+import { useParams } from "react-router-dom";
+const Booknoteupdate = () => {
+  const dispatch = useDispatch();
+  const [imageList, setImageList] = useState([]);
+  const navigate = useNavigate();
+  const [postData, setPostData] = useState(null); // 추가: 수정할 데이터 상태
+  const [isEditMode, setIsEditMode] = useState(false); // 추가: 수정 모드 상태
+  const [selectedBook, setSelectedBook] = useState(null);
+  const { no } = useParams();
+  const handleBookSelect = (book) => {
+    setSelectedBook(book);
+  };
+
+  const [movieContent, setMovieContent] = useState({
+    title: "",
+    content: "",
+  });
+
+  useEffect(() => {
+    axios.get(`/api/notelist/${no}`).then((res) => setPostData(res.data)); // 추가: 수정할 데이터 가져오기
+  }, [no]);
+ 
+  const submitReview = (e) => {
+    e.preventDefault();
+    let body = {
+      title: movieContent.title,
+      content: movieContent.content,
+      thumbnail: selectedBook.thumbnail,
+      url: selectedBook.url,
+      bookcontents: selectedBook.contents,
+      booktitle: selectedBook.title,
+      publisher: selectedBook.publisher,
+      authors: selectedBook.authors,
+      bookdatetime: selectedBook.datetime,
+    };
+    dispatch(noteList(body)).then((response) => {
+      if (response.payload.success) {
+        alert("등록완료");
+        navigate("/booknote");
+      }
+    });
+  };
+
+  const getValue = (e) => {
+    const { name, value } = e.target;
+    setMovieContent({
+      ...movieContent,
+      [name]: value,
+    });
+  };
+  const onChangeImageInput = (e) => {
+    setImageList([...imageList, ...e.target.files]);
+  };
+  return (
+    <div >
+      <Form onSubmit={submitReview}>
+      <h1 style={{marginLeft:"20px"}}>독서 노트 만들기</h1>
+      <Libray onBookSelect={handleBookSelect} />
+        <div className="App">
+        
+
+          <div className="form-wrapper">
+            <input
+              className="title-input"
+              type="text"
+              placeholder="제목"
+              onChange={getValue}
+              name="title"
+              style={{ width: "500px" }}
+            />
+            <input
+              type="file"
+              accept="image/jpg,image/png,image/jpeg,image/gif"
+              multiple
+              onChange={onChangeImageInput}
+            />
+            <CKEditor
+              editor={ClassicEditor}
+              data="<p>Hello from CKEditor 5!</p>"
+              onReady={(editor) => {
+                // You can store the "editor" and use when it is needed.
+                console.log("Editor is ready to use!", editor);
+              }}
+              onChange={(event, editor) => {
+                const data = editor.getData();
+                console.log({ event, editor, data });
+                setMovieContent({
+                  ...movieContent,
+                  content: data,
+                });
+              }}
+              onBlur={(event, editor) => {
+                console.log("Blur.", editor);
+              }}
+              onFocus={(event, editor) => {
+                console.log("Focus.", editor);
+              }}
+            />
+          </div>
+         
+          <Button variant="primary" type="submit">
+           수정완료
+          </Button>
+           
+          <Button variant="primary" type="submit">
+           수정 취소
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
+};
+
+export default Booknoteupdate;
