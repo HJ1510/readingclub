0f137a4281d1aa70bcb68a69c25a60d94d84ff57
HJ1510 <yhj221004@gmail.com>
2023-02-28 17:58:02 +0900

    css: main

--- src/component/main/index.js
+++ src/component/main/index.js
@@ -1,29 +1,39 @@
 import Layout from "../../layout/Layout";
-import axios from "axios";
 import book1 from "assets/images/book1.jpg";
 import book2 from "assets/images/book2.jpg";
-import book3 from "assets/images/meetingsample.jpg";
+import book3 from "assets/images/book3.jpg";
+import book4 from "assets/images/book4.jpg";
+import book5 from "assets/images/book5.jpg";
 import "assets/css/component/main/main.css";
-import React, { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 const images = [
   {
-    title: "사진 제목 1",
+    title: "페스트",
     url: book1,
   },
   {
-    title: "사진 제목 2",
+    title: "오만과 편견",
     url: book2,
   },
   {
-    title: "사진 제목 3",
+    title: "달러구트 꿈 백화점",
     url: book3,
   },
-  // 이하 생략
+  {
+    title: "파친코",
+    url: book4,
+  },
+  {
+    title: "당신 인생의 이야기",
+    url: book5,
+  },
 ];
 
 function Main() {
   const [currentImage, setCurrentImage] = useState({});
+  const [nextImage, setNextImage] = useState({});
+  const imageRef = useRef(null);
 
   useEffect(() => {
     const randomIndex = Math.floor(Math.random() * images.length);
@@ -31,28 +41,25 @@ function Main() {
 
     const intervalId = setInterval(() => {
       const randomIndex = Math.floor(Math.random() * images.length);
-      setCurrentImage(images[randomIndex]);
+      setNextImage(images[randomIndex]);
+      imageRef.current.classList.add("next");
+      setTimeout(() => {
+        setCurrentImage(images[randomIndex]);
+        imageRef.current.classList.remove("next");
+      }, 600);
     }, 2300);
 
     return () => clearInterval(intervalId);
   }, []);
 
-  function user() {
-    return axios
-      .get("https://jsonplaceholder.typicode.com/users")
-      .then((Response) => {
-        console.log(Response.data[2].name);
-      })
-      .catch((Error) => {
-        console.log(Error);
-      });
-  }
-
   return (
     <Layout>
       <div className="image-wrapper">
-        <img src={currentImage.url} alt={currentImage.title} />
-        <h2>{currentImage.title}</h2>
+        <div className="image-info">
+          <h2>{currentImage.title}</h2>
+        </div>
+        <img src={currentImage.url} alt={currentImage.title} ref={imageRef} />
+        <img src={nextImage.url} alt={nextImage.title} className="next" />
       </div>
     </Layout>
   );
