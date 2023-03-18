e6c65983d834534606ecbef71603b6959d65dfc1
HJ1510 <yhj221004@gmail.com>
2023-03-18 14:14:27 +0900

    css: mymeeting 공백

--- src/component/meeting/MyMeeting.js
+++ src/component/meeting/MyMeeting.js
@@ -1,9 +1,10 @@
-import Layout from "layout/Layout";
-import { Col, Row } from "react-bootstrap";
-import MeetingCalender from "./MeetingCalender";
-import { useEffect, useState } from "react";
-import { meetingList } from "MeetigData";
-import MyMeetingList from "./MyMeetingList";
+import Layout from 'layout/Layout';
+import { Col, Row } from 'react-bootstrap';
+import MeetingCalender from './MeetingCalender';
+import { useEffect, useState } from 'react';
+import { meetingList } from 'MeetigData';
+import MyMeetingList from './MyMeetingList';
+import styles from 'assets/css/component/meeting/Meeting.module.css';
 
 function MyMeeting() {
   const [mockList, setMockList] = useState([]);
@@ -14,10 +15,10 @@ function MyMeeting() {
 
   return (
     <Layout>
-      <div className="MyMeeting">
+      <div className={styles.MyMeeting}>
         <Row>
           <Col md={7}>
-            <MeetingCalender className="Calender" />
+            <MeetingCalender className={styles.Calender} />
           </Col>
           <Col md={5}>
             <MyMeetingList />
