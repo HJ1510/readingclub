9f031867e81c30befc79c90f4347280fd734555e
HJ1510 <yhj221004@gmail.com>
2023-03-24 15:30:27 +0900

    fix: main

--- src/component/main/index.js
+++ src/component/main/index.js
@@ -1,32 +1,32 @@
-import Layout from "../../layout/Layout";
-import book1 from "assets/images/book1.jpg";
-import book2 from "assets/images/book2.jpg";
-import book3 from "assets/images/book3.jpg";
-import book4 from "assets/images/book4.jpg";
-import book5 from "assets/images/book5.jpg";
-import "assets/css/component/main/main.css";
-import { useState, useEffect, useRef } from "react";
-import { Col, Row } from "react-bootstrap";
+import Layout from '../../layout/Layout';
+import book1 from 'assets/images/book1.jpg';
+import book2 from 'assets/images/book2.jpg';
+import book3 from 'assets/images/book3.jpg';
+import book4 from 'assets/images/book4.jpg';
+import book5 from 'assets/images/book5.jpg';
+import 'assets/css/component/main/main.css';
+import { useState, useEffect, useRef } from 'react';
+import { Col, Row } from 'react-bootstrap';
 
 const images = [
   {
-    title: "페스트",
+    title: '페스트',
     url: book1,
   },
   {
-    title: "오만과 편견",
+    title: '오만과 편견',
     url: book2,
   },
   {
-    title: "달러구트 꿈 백화점",
+    title: '달러구트 꿈 백화점',
     url: book3,
   },
   {
-    title: "파친코",
+    title: '파친코',
     url: book4,
   },
   {
-    title: "당신 인생의 이야기",
+    title: '당신 인생의 이야기',
     url: book5,
   },
 ];
@@ -65,19 +65,25 @@ function Main() {
   }, [nextImage]);
 
   useEffect(() => {
-    if (currentImageRef.current && nextImageRef.current) {
-      currentImageRef.current.classList.add("next");
-      nextImageRef.current.classList.add("prev");
-      setTimeout(() => {
-        currentImageRef.current.classList.remove("next");
-        nextImageRef.current.classList.remove("prev");
+    if (
+      currentImageRef.current &&
+      nextImageRef.current &&
+      currentImageRef.current.classList
+    ) {
+      currentImageRef.current.classList.add('next');
+      nextImageRef.current.classList.add('prev');
+      const timeoutId = setTimeout(() => {
+        currentImageRef.current.classList.remove('next');
+        nextImageRef.current.classList.remove('prev');
       }, 1000);
+
+      return () => clearTimeout(timeoutId);
     }
   }, [currentImage, nextImage]);
 
   return (
     <Layout>
-      <Row className="image-wrapper">
+      <Row className='image-wrapper'>
         <Col md={1}></Col>
         <Col md={2}>
           <h2>Hello</h2>
@@ -93,7 +99,7 @@ function Main() {
         <Col>
           <img src={nextImage.url} alt={nextImage.title} ref={nextImageRef} />
         </Col>
-        <Col md={2} className="image-info">
+        <Col md={2} className='image-info'>
           <h2>Bye</h2>
           <h3>{nextImage.title}</h3>
         </Col>
