1675dd68254bf5e6276c7307d1a5c45bc30cd42a
HJ1510 <yhj221004@gmail.com>
2023-03-28 19:31:43 +0900

    fix: 조금

--- src/component/meeting/MeetingGroup.js
+++ src/component/meeting/MeetingGroup.js
@@ -20,7 +20,7 @@ function MeetingGroup() {
   const [showModal, setShowModal] = useState(false);
   const members = useMembers(no);
   const meetingInfo = useMeetingInfo(no);
-  console.log(meetingInfo)
+  console.log(meetingInfo);
   // const [messages, setMessages] = useState([]);
 
   // useEffect(() => {
@@ -38,27 +38,23 @@ function MeetingGroup() {
   return (
     <Layout className={styles.meeting}>
       <Container>
-       
-      
-       
         <Row>
-        <section className="py-5 text-center container">
-    <div className="row py-lg-5">
-      <div className="col-lg-6 col-md-8 mx-auto">
-        <h1 className="fw-light">{meetingInfo.title}</h1>
-        <p className="lead text-muted">{meetingInfo.introduce}</p>
-     
-        {meetingInfo.order?.map((order) => (
-                 <p className="lead text-muted">일정 : {order.date}</p>
+          <section className='py-5 text-center container'>
+            <div className='row py-lg-5'>
+              <div className='col-lg-6 col-md-8 mx-auto'>
+                <h1 className='fw-light'>{meetingInfo.title}</h1>
+                <p className='lead text-muted'>{meetingInfo.introduce}</p>
+
+                {meetingInfo.order?.map((order) => (
+                  <p className='lead text-muted'>일정 : {order.date}</p>
                 ))}
-        <p className="lead text-muted">장소 : {meetingInfo.location}</p>
-        <p>
-         
-          <Button onClick={() => handlechat(no)}>채팅 열기</Button>
-        </p>
-      </div>
-    </div>
-  </section>
+                <p className='lead text-muted'>장소 : {meetingInfo.location}</p>
+                <p>
+                  <Button onClick={() => handlechat(no)}>채팅 열기</Button>
+                </p>
+              </div>
+            </div>
+          </section>
           <Col>
             <MeetingCalender
               apiFunction={getOrdersByNo}
@@ -67,39 +63,44 @@ function MeetingGroup() {
             />
           </Col>
         </Row>
-  <h1 style={{margin:"20px"}}>    맴버 현황</h1>
-  <div className="row row-cols-1 row-cols-sm-2 row-cols-md-4 g-4">
-
-        {members &&
-          members.map((member, idx) => {
-            return (
- 
-     <div className="col" key={idx} >
-  
-       <div className="card shadow-sm">
-       <img  style={{height:"200px"}}src={`/${member.imgpath.path}`} alt='member' />
-       <strong  style={{margin:"10px"}}class=" text-success"> {member.role}</strong>
-         <div class="card-body">
-       
-           <strong className="card-text" style={{fontSize:"1.5em"}}>{member.name}</strong>
-      
-   
-         <p>{member.status}</p>
-           <div className="d-flex justify-content-between align-items-center">
-             <div className="btn-group">
-              
-               {/* <button type="button" className="btn btn-sm btn-outline-secondary">승인</button>
+        <h1 style={{ margin: '20px' }}>Members</h1>
+        <div className='row row-cols-1 row-cols-sm-2 row-cols-md-4 g-4'>
+          {members &&
+            members.map((member, idx) => {
+              return (
+                <div className='col' key={idx}>
+                  <div className='card shadow-sm'>
+                    <img
+                      style={{ height: '200px' }}
+                      src={`/${member.imgpath.path}`}
+                      alt='member'
+                    />
+                    <strong style={{ margin: '10px' }} class=' text-success'>
+                      {member.role}
+                    </strong>
+                    <div class='card-body'>
+                      <strong
+                        className='card-text'
+                        style={{ fontSize: '1.5em' }}
+                      >
+                        {member.name}
+                      </strong>
+                      <p>{member.status}</p>
+                      <div className='d-flex justify-content-between align-items-center'>
+                        <div className='btn-group'>
+                          {/* <button type="button" className="btn btn-sm btn-outline-secondary">승인</button>
                <button type="button" className="btn btn-sm btn-outline-secondary">거부</button> */}
-             </div>
-             <small className="text-muted">{meetingInfo.createdAt}</small>
-           </div>
-         </div>
-       </div>
-     </div>
-  
-            );
-          })}
-   </div>
+                        </div>
+                        <small className='text-muted'>
+                          {meetingInfo.createdAt}
+                        </small>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
         <Row>
           <Board title='meetingBoard' />
         </Row>
